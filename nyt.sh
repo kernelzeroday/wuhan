ca21ad891406d6f27462ff39ca14d89efe6b81d9
kod <kod@m.local>
2020-03-05 10:31:12 -0800

    update

--- nyt.sh
+++ nyt.sh
@@ -1,3 +1,4 @@
+curl 'https://static01.nyt.com/newsgraphics/2020/01/21/china-coronavirus/43232c77c4a0b1840abb3449a58cca018fe3e518/build/js/chunks/model.js' > model.mjs
 node nyt2 | jq '.u' | in2csv -f json > nyt_us_full.csv
 csvstack -g "`date +%s`" -n unixtime nyt_us_full.csv |csvstack -g "`date`" -n date2 | tee -a nyt2/nyt_us_full_$(date +%s).csv
 node nyt2 | jq '.b' | in2csv -f json > nyt_us_county.csv
