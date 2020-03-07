07c95cd54d669eda0272d3348ba4690f26d702ab
kod <kod@m.local>
2020-03-06 20:33:37 -0800

    update

---
+++ death_rates/death_rates.sh
@@ -0,0 +1,3 @@
+htmltab -s 1 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/' > 1.csv
+htmltab -s 2 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/' > 2.csv
+htmltab -s 3 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/' > 3.csv
