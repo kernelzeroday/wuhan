5254e729e1a9999ce66b1c84dcf210091ac9cf96
kod <kod@m.local>
2020-03-11 01:35:45 -0700

    update

--- az/az.sh
+++ az/az.sh
@@ -1,3 +1,4 @@
+#pulled from https://tableau.azdhs.gov/views/COVID-19Table/COVID-19table?:isGuestRedirectFromVizportal=y&:embed=y
 curl -s 'https://tableau.azdhs.gov/vizql/w/COVID-19Table/v/COVID-19table/vud/sessions/1266E487E5F94CAB913DBC79AE32FA47-1:0/views/8275719771277684273_10329084236957726728?csv=true&showall=true' |tee travel_person$(date +%s).csv | xsv table
 echo ''
 curl -s 'https://tableau.azdhs.gov/vizql/w/COVID-19Table/v/COVID-19table/vud/sessions/1266E487E5F94CAB913DBC79AE32FA47-1:0/views/8275719771277684273_133021235305994309?csv=true&showall=true' |tee total_cases_death$(date +%s).csv | xsv table
