8b56bbdc476b47ce1b5fb19f571b0720d004ee2c
kod <kod@m.local>
2020-03-12 22:19:14 -0700

    update

---
+++ py/jhu.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('time_series_19-covid-Confirmed.csv')
+df = df[['Province/State' , 'Country/Region', 'Lat', 'Long',  df.columns[-2], df.columns[-1]]] 
+df['Delta'] = (df[df.columns[-1]]  - df[df.columns[-2]])
+df['Delta'] = df.columns[-1] - df.columns[-2] 
+df['Percentage'] = df['Delta'] / df[df.columns[-3]] 
+df['Percentage'] = df['Percentage'] * 100 
+ 
+df['date']  = pd.to_datetime('today')
+
+df.round(8).to_csv('output/jhu_us_delta_processed_' + str(int(time.time()) ) + '.csv', float_format='%.8f')
+df.round(8).to_csv('output/jhu_us_delta_processed.csv', float_format='%.8f')
+
